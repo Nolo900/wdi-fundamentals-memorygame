91b6b340878ed695f0d072218e305d9ce984f354
Austin Miles <abm@austinbmiles.com>
2016-10-20 21:28:47 -0400

    added card logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,5 +5,13 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-
+if (cardOne === cardTwo) {
+	alert("Match!");
+} else if (cardThree === cardFour){
+	alert("Match!");
+} else if (cardOne === cardThree){
+	alert("Sorry, try again.");
+} else if(cardTwo === cardFour){
+	alert("Sorry, try again.");
+}
 
