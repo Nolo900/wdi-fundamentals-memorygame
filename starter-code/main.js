52f77cafd786eaa85bfe7fd73eacb95fe80e6570
Austin Miles <abm@austinbmiles.com>
2016-10-27 15:54:06 -0400

    added js to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,17 +1,27 @@
-console.log("We're connected!")
+// console.log("We're connected!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert("Match!");
-} else if (cardThree === cardFour){
-	alert("Match!");
-} else if (cardOne === cardThree){
-	alert("Sorry, try again.");
-} else if(cardTwo === cardFour){
-	alert("Sorry, try again.");
+// if (cardOne === cardTwo) {
+// 	alert("Match!");
+// } else if (cardThree === cardFour){
+// 	alert("Match!");
+// } else if (cardOne === cardThree){
+// 	alert("Sorry, try again.");
+// } else if(cardTwo === cardFour){
+// 	alert("Sorry, try again.");
+// }
+function createCards(){
+	var myGameBoard = document.getElementById('game-board');
+
+	for (i=0;i<4;i++){
+		var cardDiv = document.createElement('div');
+		cardDiv.className = "card";
+		myGameBoard.appendChild(cardDiv);
+	}	
 }
 
+createCards();
